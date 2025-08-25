415affc8f447d112c3d94202044a9cf3b7f17c61
mufeedha-tm <mufeedha059@gmail.com>
2025-08-25 13:10:31 +0530

    first commit

---
+++ var.js
@@ -0,0 +1,12 @@
+// syntax error mattan "use strict" use cheyya
+
+// var il redeclare cheyyanum reassign cheyya& functio-scoped
+
+// let-block-scoped reassign not redeclare
+
+// constant-block-scoped
+
+var x="musi";
+var x="midhu";
+console.log(x);
+
