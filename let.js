1a756c5c43bbb15f866848657c7aeafd9a072339
mufeedha-tm <mufeedha059@gmail.com>
2025-08-26 13:07:16 +0530

    first commit

---
+++ let.js
@@ -0,0 +1,9 @@
+// let y="fami";
+// let x="niya";
+// x=y;
+
+let x="fami"
+x="rahan"
+console.log(x);
+
+// re-declaration is not possible but re-assigning is possible in Let
--- var.js
+++ var.js
@@ -1,12 +1,16 @@
 // syntax error mattan "use strict" use cheyya
 
-// var il redeclare cheyyanum reassign cheyya& functio-scoped
+// var il redeclare cheyyanum reassign cheyya& function-scoped
 
 // let-block-scoped reassign not redeclare
 
 // constant-block-scoped
 
-var x="musi";
-var x="midhu";
-console.log(x);
+// var x="musi";
+// var x="midhu";
+// console.log(x);
 
+var x = "mufi";
+var y = "mithra";
+x = y;
+console.log(x);
